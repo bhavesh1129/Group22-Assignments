b0c4d1175ada5b436c2095c3efef13e917fe92f6
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-21 09:55:05 +0530

    Update IncreasingDecreasingSequence.cpp

--- CPP_Assignments/Assignment1/IncreasingDecreasingSequence.cpp
+++ CPP_Assignments/Assignment1/IncreasingDecreasingSequence.cpp
@@ -27,4 +27,63 @@ int main() {
     n--;
   }
   cout << "true";
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+  int n;
+
+  cin >> n;
+
+  int e, p, inc = 0;
+
+  cin >> p;
+
+  n--;
+
+  while (n > 0) {
+
+    cin >> e;
+
+    if (e == p) {
+
+      cout << "false";
+
+      return 0;
+
+    }
+
+    if (e < p && inc == 0) {
+
+      n--;
+
+      p = e;
+
+      continue;
+
+    } else if (e < p && inc == 1) {
+
+      cout << "false";
+
+      return 0;
+
+    } else if (e > p) {
+
+      inc = 1;
+
+    }
+
+    p = e;
+
+    n--;
+
+  }
+
+  cout << "true";
+
+  return 0;
+
 }
+
