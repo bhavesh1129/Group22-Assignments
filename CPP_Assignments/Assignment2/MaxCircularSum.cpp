5a1c28a1e7137f7e31182e0ae61ae5faa9982ec3
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-29 01:25:56 +0530

    Add Solution

--- CPP_Assignments/Assignment2/MaxCircularSum.cpp
+++ CPP_Assignments/Assignment2/MaxCircularSum.cpp
@@ -1,40 +1,50 @@
-#include <iostream>
+#include<iostream>
+#include<cmath>
+#include<climits>
 using namespace std;
 
-int kadane(int a[],int n)
-{
-    int curSum=0;
-    int maxSum=0;
-    for(int i=0;i<n;i++){
-        curSum = curSum + a[i];
-        if(curSum < 0){
-            curSum = 0;
+int kadane(int arr[],int n){
+	int max_so_far = arr[0], max_ending = arr[0];
+
+	for (int i = 1; i < n; i++)
+	{
+
+        if (arr[i] <= max_ending + arr[i]) {
+            max_ending += arr[i];
         }
-        maxSum = max(curSum,maxSum);
-    }
-    return maxSum;
-}
-int cSum(int a[], int n){
-    int c1 = kadane(a, n);
-    int c2 = 0;
-    for(int i = 0; i < n; i++){
-        c2 += a[i];
-        a[i] = -a[i];
-    }
-    c2 = c2 + kadane(a, n);
-    return (c2 > c1) ? c2 : c1; 
+       
+        else {
+            max_ending = arr[i];
+        }
+		// max_ending = max_ending + arr[i];
+		// if (max_ending < 0)
+		// 	max_ending = 0;
+		if (max_so_far < max_ending)
+			max_so_far = max_ending;
+
+		
+	}
+	return max_so_far;
 }
 int main() {
-    int t;
-    cin >> t;
-    for(int i = 0;i < t; i++){
-        int a[1000];
-        int n;
-        cin >> n;
-        for(int i=0;i<n;i++){
-            cin >> a[i];
-        }
-        cout << cSum (a, n);
-    }
-    return 0;
+	int t;
+	cin>>t;
+	for(int k=0;k<t;k++){
+
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++){
+			cin>>arr[i];
+		}
+		int result1=kadane(arr,n);
+		int sum=0;
+		for(int i=0;i<n;i++){
+			sum+=arr[i];
+			arr[i]=-arr[i];
+		}
+		int result2=kadane(arr,n);
+		cout<<max(result1,sum+result2)<<endl;
+	}
+	return 0;
 }
