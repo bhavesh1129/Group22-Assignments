2fbc13769046ced26ffa9dd33e5206ee55d1d9df
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-06 02:24:56 +0530

    Add Solution

--- CPP_Assignments/Assignment4/TripletHavingMaximumProduct.cpp
+++ CPP_Assignments/Assignment4/TripletHavingMaximumProduct.cpp
@@ -12,7 +12,7 @@ int main() {
     sort(arr,arr+num);
     
     if((arr[0]*arr[1]*arr[num-1])>(arr[num-1]*arr[num-2]*arr[num-3])){
-        cout<<arr[0]<<" "<<arr[1]<<" "<<arr[num-1]<<endl;
+        cout<<arr[num-1]<<" "<<arr[1]<<" "<<arr[0]<<endl;
     }else{
         cout<<arr[num-1]<<" "<<arr[num-2]<<" "<<arr[num-3]<<endl;
     }
