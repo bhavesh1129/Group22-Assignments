fa72a0ed609dfbf2566e73ed996e6e64158ffe18
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-05 17:55:19 +0530

    Add Solution

---
+++ CPP_Assignments/Assignment4/TripletHavingMaximumProduct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num;
+    cin>>num;
+    int arr[num];
+    for(int i=0;i<num;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+num);
+    
+    if((arr[0]*arr[1]*arr[num-1])>(arr[num-1]*arr[num-2]*arr[num-3])){
+        cout<<arr[0]<<" "<<arr[1]<<" "<<arr[num-1]<<endl;
+    }else{
+        cout<<arr[num-1]<<" "<<arr[num-2]<<" "<<arr[num-3]<<endl;
+    }
+}
