4d221095c2915ed906e15409a04234970a29b18a
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-09-02 03:16:10 +0530

    Update script.js

--- Ajax-Api Assignmnet/Assignment1/script.js
+++ Ajax-Api Assignmnet/Assignment1/script.js
@@ -1,1 +1,31 @@
+// Enter Button
 
+    //Using fetch method to fetch the data from open weather API
+
+    //Covert data from string format to JSON format
+  
+        //The whole data
+
+            //Current Date
+
+            //Name of City
+
+            //Temp Value and convert in into Kelvin to Celsius
+
+            //Weather Type
+
+            //Weather Icon
+
+            //Changing the background image basis on their weather type
+
+            //Extra Weather details (Min Temperature)
+
+            //Extra Weather details (Max Temperature)
+
+            //Some More Extra Weather details (Humidity)
+
+            //Some More Extra Weather details (pressure)
+      
+            //Empty Input box
+      
+        //To catch error if present and show
