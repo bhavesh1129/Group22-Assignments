9f8368b7988fe21b404e6c3b9df1994f6fc05104
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-10-30 18:28:12 +0530

    Update Histogram.java

--- Java _Assignments/Assignment1/Histogram.java
+++ Java _Assignments/Assignment1/Histogram.java
@@ -6,17 +6,17 @@ public class Histogram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] ht = new int[n];
+        long[] ht = new long[n];
         for (int i = 0; i < ht.length; i++) {
             ht[i] = sc.nextInt();
         }
         histogram(ht);
     }
 
-    private static void histogram(int[] ht) {
+    private static void histogram(long[] ht) {
         //NSE
         Stack<Integer> st1 = new Stack<>();
-        int[] nse = new int[ht.length];
+        long[] nse = new long[ht.length];
         st1.push(0);
         for (int i = 0; i < ht.length; i++) {
             while (!st1.isEmpty() && ht[st1.peek()] > ht[i]) {
@@ -31,7 +31,7 @@ public class Histogram {
         //PSE
         Stack<Integer> st = new Stack<>();
         st.push(0);
-        int[] pse = new int[ht.length];
+        long[] pse = new long[ht.length];
         pse[0] = -1;
 
         for (int i = 1; i < ht.length; i++) {
@@ -46,9 +46,9 @@ public class Histogram {
             st.push(i);
         }
 
-        int maxArea = Integer.MIN_VALUE;
+        long maxArea = Integer.MIN_VALUE;
         for (int i = 0; i < ht.length; i++) {
-            int area = ht[i] * (nse[i] - pse[i] - 1);
+            long area = ht[i] * (nse[i] - pse[i] - 1);
             maxArea = Math.max(area, maxArea);
         }
         System.out.println(maxArea);
